c804092e21f64011201be41f328a966203085fe4
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-01-08 11:17:59 -0600

    Can display todos

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,34 @@
 <script>
-	import Header from './components/Header.svelte'
-	import Footer from './components/Footer.svelte'
-	export let name;
-</script>
+	import { onMount } from "svelte";
 
-<main>
-	<Header/>
-	<h1>{name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<Footer/>
-</main>
+	// Variable to hold todos
+	let todos = [];
+
+	// Base Url
+	const baseUrl = "http://localhost:3000/todos";
+
+	// Get todos from backend
+	const getTodos = async () => {
+		const response = await fetch(baseUrl);
+		const data = await response.json();
+		todos = await data;
+	};
+
+	// I believe this runs when the component loads like React.useEffect
+	onMount(() => {
+		getTodos();
+	});
+</script>
 
 <style>
 </style>
+
+<main>
+	<h1>The Svelte To Do App</h1>
+	{#each todos as todo}
+		<div>
+			<h2>{todo.title}</h2>
+			<h2>{todos.body}</h2>
+		</div>
+	{/each}
+</main>
