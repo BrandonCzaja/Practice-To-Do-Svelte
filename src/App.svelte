67dd709a46b31da60e76f216e81bedc2f7db71f5
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-01-08 11:28:04 -0600

    Can now create todos

--- src/App.svelte
+++ src/App.svelte
@@ -14,10 +14,36 @@
 		todos = await data;
 	};
 
-	// I believe this runs when the component loads like React.useEffect
+	// Runs when the component loads like React.useEffect
 	onMount(() => {
 		getTodos();
 	});
+
+	// Properties of Create Form
+	let createTitle;
+	let createBody;
+
+	// Function for Create Form submission
+	const createTodo = async (event) => {
+		event.preventDefault();
+		await fetch(baseUrl, {
+			method: "post",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				title: createTitle,
+				body: createBody,
+			}),
+		});
+
+		// Refresh to list of todos
+		getTodos();
+
+		// Reset the form
+		createTitle = "";
+		createBody = "";
+	};
 </script>
 
 <style>
@@ -25,10 +51,19 @@
 
 <main>
 	<h1>The Svelte To Do App</h1>
+	<hr />
+	<h2>Create a To Do</h2>
+	<form on:submit={createTodo}>
+		<input type="text" bind:value={createTitle} />
+		<input type="text" bind:value={createBody} />
+		<input type="submit" value="Create a To Do" />
+	</form>
+	<hr />
+	<h2>The To Dos</h2>
 	{#each todos as todo}
 		<div>
 			<h2>{todo.title}</h2>
-			<h2>{todos.body}</h2>
+			<h3>{todo.body}</h3>
 		</div>
 	{/each}
 </main>
